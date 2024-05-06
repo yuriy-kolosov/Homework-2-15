55c443d9bf16311d59c1834bd5d2d9359f427997
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-05-06 23:20:05 +0300

    Домашнее задание 2-15. Основы алгоритмов. Часть 1 - начальный коммит

---
+++ src/main/java/pro/sky/homework215/Main.java
@@ -0,0 +1,9 @@
+package pro.sky.homework215;
+
+public class Main {
+    public static void main(String[] args) {
+//                                              Домашнее задание 2-15. Основы алгоритмов. Часть 1
+        System.out.println("Домашнее задание 2-15. Основы алгоритмов. Часть 1");
+        System.out.println();
+    }
+}
